f1b93ef5cf9e07b875ff5e26ae5fab2230c1cdfe
Barso2 <reznitskyalex@gmail.com>
2023-03-13 13:12:16 +0200

    app_01 react first application

---
+++ app_01/src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <p className="text">Hello react text</p>
+      <select name="" id="">
+        <option value="1">one</option>
+        <option value="2">two</option>
+      </select>
+      <hr />
+      <img src="/images/react.png" alt="react" title='react' />
+      <div>react div</div>
+    </>
+  );
+}
+
+export default App;
