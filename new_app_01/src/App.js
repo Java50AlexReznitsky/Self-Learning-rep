89a7766341d2fcef0bc54d4173de78754090f8fa
Barso2 <reznitskyalex@gmail.com>
2023-03-24 10:45:43 +0300

    updated with useState useEffect hooks

--- new_app_01/src/App.js
+++ new_app_01/src/App.js
@@ -1,56 +1,43 @@
-import React, { useState } from 'react'
-// import Counter from './components/Counter';
-// import ClassCounter from './components/ClassCounter'
-import './styles/App.css'
-import PostList from './components/PostList';
-import PostItem from './components/PostItem';
-import MyButton from './components/UI/button/MyButton';
+import React, { useState, useEffect } from 'react'
 
 
 function App() {
-  // const [likes, setLikes] = useState(5)
-  // const [value, setValue] = useState('TEXT IN INPUT')
-  // let likes = 5;        // wont work since virtual DOM doesn't know that something changed, we need useState()
-  // function increment(){
-  //   likes += 1;
-  //   console.log(likes)
-  // }
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'JavaScript', body: 'Description' },
-    { id: 3, title: 'JavaScript', body: 'Description' },
-    { id: 4, title: 'JavaScript', body: 'Description' },
-  ])
-  // const [posts2, setPosts2] = useState([
-  //   { id: 1, title: 'Python', body: 'Description' },
-  //   { id: 2, title: 'Python', body: 'Description' },
-  //   { id: 3, title: 'Python', body: 'Description' },
-  //   { id: 4, title: 'Python', body: 'Description' },
-  // ])
+  // const [resourceType, setResourceType] = useState('posts');
+  // const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then(response => response.json())
+  //     .then(json => setItems(json))
+
+  // }, [resourceType])
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, []);
 
   return (
-    <div className="App">
-      {/* <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      /> */}
-      {/* <Counter /> */}
-      {/* <ClassCounter /> */}
-      {/* <PostItem post={{ id: 1, title: 'JavaScript', body: 'Description' }} />
-      <PostItem post={{ id: 2, title: 'JavaScript', body: 'Description' }} />
-      <PostItem post={{ id: 3, title: 'JavaScript', body: 'Description' }} />
-      <PostItem post={{ id: 4, title: 'JavaScript', body: 'Description' }} /> */}
-      {/* <PostList posts={posts2} title='Posts about Python' /> */}
-      <form>
-        <input type="text" placeholder='post name' />
-        <input type="text" placeholder='post description' />
-        <MyButton disabled>create post</MyButton>
-      </form>
-      <PostList posts={posts} title='Posts about JS' />
-    </div>
+    <>
+      <h1>
+        {windowWidth}
+
+      </h1>
+      {/* <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })} */}
+    </>
   );
 }
 
