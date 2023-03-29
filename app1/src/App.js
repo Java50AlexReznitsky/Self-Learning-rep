93bb56c45987bfca2bc05088f725256fa67b3da5
Barso2 <reznitskyalex@gmail.com>
2023-03-30 02:44:59 +0300

    subSpindle calculation prog

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,11 +1,20 @@
 import React, { useState, useRef, useEffect } from "react";
 import Functions from "./service/Functions";
+import './styles/App.css'
 
 function App() {
+  const inputRef = useRef('').current.value
+  const inputRef1 = useRef('').current.value
+  const [number, setNumber] = useState(inputRef)
+  const [number2, setNumber2] = useState(inputRef1)
+  const numbArr = [number, number2]
   return (
-    <div>
-    <h1>Current number</h1>
-    <Functions props={2.34}/>
+    <div className="mainDiv">
+      <h1>Calculate number</h1>
+      <input ref={inputRef} className="inputStyle" onChange={e => setNumber(e.target.value)} placeholder="Set initial number" type="number" />
+      <input onChange={e => setNumber2(e.target.value)} placeholder="Set calc number" type="number" />
+      <Functions props={numbArr} />
+
 
     </div>
   )
--- app1/src/service/Functions.jsx
+++ app1/src/service/Functions.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export default function Functions({props}) {
-  return (
-    <div>
-      <h1>{43 - props}</h1>
-    </div>
-  )
+export default function Functions({ props }) {
+    const show =
+        <div>
+            <h1 className='output'>{`${props[0]} - ${props[1]} = ${props[0] - props[1]}`}</h1>
+        </div>
+    const hide = <h2>Input some numbers</h2>
+    const intermediateShow = <h1>One more to go</h1>
+    let showCalc = undefined
+    if (props[0] && props[1]) {
+        showCalc = show
+    } else if (props[0] || props[1]) {
+        showCalc = intermediateShow;
+    } else { showCalc = hide; }
+
+    return (
+        <>
+            {showCalc}
+        </>
+    )
 }
